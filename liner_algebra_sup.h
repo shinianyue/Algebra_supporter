afa3a8a6af12e8258638579c9f369a2c93fb24d6
Graff_sny <92242741+shinianyue@users.noreply.github.com>
2023-09-27 22:51:15 +0800

    Reformed some structs

--- liner_algebra_sup.h
+++ liner_algebra_sup.h
@@ -6,47 +6,107 @@
 //----------------------------------------
 #pragma once
 #include <vector>
+using std::vector;
 namespace AlgebraSup
 {
-	template<typename T>
-	class Vec
+	struct Size_rect
 	{
-	private: std::vector<T>_datas;
-			 int _length;//resorved
-	public: void append(T input)//inset a new data;
-	{
-		_datas.push_back(input);
-	}
-			int length()//return the lenth of the vector;
-	{
-		return(_datas.size());
-	}
-			Vec<T> operator+(Vec<T> _another)
+		int x;
+		int y;
+		bool operator==(Size_rect another_size) {
+			if (another_size.x == this->x && another_size.y == this->y) 
 			{
-				if (this->length() != _another.length())
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		bool operator!=(Size_rect another_size) {
+			if (another_size == *this)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	};
+	template<typename T>
+	class Matrix
+	{
+	private: vector<vector<T>>_datas;
+			 int _length;//resorved
+			 Size_rect size_rect = {0,0};
+			 void flush_size() {
+				 size_rect.y = _datas.size();
+				 if (size_rect.y == 0)
+				 {
+					 size_rect.x = 0;
+				 }
+				 else
+				 {
+					 size_rect.x = _datas[0].size();
+				 }
+			 }
+	public: void append(vector<T> input)
+			{
+				_datas.push_back(input);
+				flush_size();
+			}
+			int size_x()//return the lenth of the vector;
+			{
+				return(_datas.size());
+			}
+			Matrix<T> operator+(Matrix<T> _anotherMatrix)
+			{
+				if (this->size_rect != _anotherMatrix.size_rect)
 				{
-					return Vec<T>(-1);
+					Matrix<T> _tempMatrix;
+					
+					return _tempMatrix;
 				}
 				else
 				{
-					std::vector<T> temp;
-					for (int Po = 0; Po < this->length(); Po++)
+					vector<T> _tempVector;
+					vector<vector<T>> _temp2dVector;
+					for (size_t _y = 0; _y < this->size_rect.y; _y++)
 					{
-						temp.push_back(this->_datas[Po] + _another._datas[Po]);
+						for (size_t _x = 0; _x < this->size_rect.x; _x++)
+						{
+							_tempVector.push_back(_datas[_x][_y] + _anotherMatrix._datas[_x][_y]);
+						}
+						_temp2dVector.push_back(_tempVector);
+						_tempVector.clear();
 					}
-					Vec<T> result(temp);
-					return result;
+					Matrix<T> _tempMatrix;
+					_tempMatrix._datas = _temp2dVector;
+					_tempMatrix.flush_size();
+					return _tempMatrix;
 				}
 			}
-			Vec<T>(int error_num) 
-			{
-				_datas = { error_num };
+			
+			Matrix<T> operator*(Matrix<T> _another) {
+				if (this->size_rect != _another.size_rect)
+				{
+					Matrix<T> _tempMatrix;
+
+					return _tempMatrix;
+				}
+				else 
+				{
+					vector<T> _tempVector;
+					vector<vector<T>> _temp2dVector;
+					//next_updata:3
+					Matrix<T> _tempMatrix;
+					_tempMatrix._datas = _temp2dVector;
+					_tempMatrix.flush_size();
+					return _tempMatrix;
+				}
 			}
-			Vec<T>(std::vector<T> _numbers) 
-			{
-				_datas = _numbers;
-			}
-			Vec<T>() 
+			Matrix<T>() 
 			{
 				_length = 0;
 			}
